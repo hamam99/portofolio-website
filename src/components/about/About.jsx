29e7b6a427c4585e264eabca924147ea8933741c
Hamam <hamam.nasrodin@appfuxion.com>
2022-09-08 16:30:55 +0700

    create UI about screen

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,5 +1,53 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import './about.css'
+import ME from '../../assets/me-about.jpg'
+import { FaAward } from 'react-icons/fa'
+import { FiUser } from 'react-icons/fi'
+import { VscFolderLibrary } from 'react-icons/vsc'
+
 export default function About() {
-  return <section id="about">About</section>
+  return (
+    <section id="about">
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="About Image" />
+          </div>
+        </div>
+
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FaAward className="about__icon" />
+              <h5>Experience</h5>
+              <small>3+ Years Working</small>
+            </article>
+            <article className="about__card">
+              <FiUser className="about__icon" />
+              <h5>Clients</h5>
+              <small>200+ Worldwide</small>
+            </article>
+            <article className="about__card">
+              <VscFolderLibrary className="about__icon" />
+              <h5>Projects</h5>
+              <small>80+ Completed Project</small>
+            </article>
+          </div>
+
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book.
+          </p>
+
+          <a href="#contact" className="btn btn-primary">
+            Lets Talk
+          </a>
+        </div>
+      </div>
+    </section>
+  )
 }
