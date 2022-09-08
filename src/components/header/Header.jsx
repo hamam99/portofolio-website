4c46d700618d56634307483928bc7e5e04116af7
Hamam <hamam.nasrodin@appfuxion.com>
2022-09-08 10:28:39 +0700

    setup header component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import CTA from './CTA'
+import ME from '../../assets/me.png'
 
 function Header() {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container heade__container">
+        <h5>Hello I'm</h5>
+        <h1>Hamam Nasrodin</h1>
+        <h5 className="text-light">React Developer</h5>
+        <CTA />
+
+        <div className="me">
+          <img src={ME} alt="me" />
+        </div>
+        <a href="#contact" className="scroll__down">
+          Scroll Down
+        </a>
+      </div>
+    </header>
   )
 }
 
