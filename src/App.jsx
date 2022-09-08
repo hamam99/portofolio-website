2bcf23dccb30a135f823fe6e717a32944742747f
Hamam <hamam.nasrodin@appfuxion.com>
2022-09-08 09:44:47 +0700

    setup component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,26 @@
 import React from 'react'
+import About from './components/about/About'
+import Contact from './components/contact/Contact'
+import Experience from './components/experience/Experience'
+import Footer from './components/footer/Footer'
+import Header from './components/header/Header'
+import Nav from './components/nav/Nav'
+import Portfolio from './components/portfolio/Portfolio'
+import Services from './components/services/Services'
+import Testimonials from './components/testimonials/Testimonials'
 
 export const App = () => {
-  return <div>App</div>
+  return (
+    <>
+      <Header />
+      <Nav />
+      <About />
+      <Experience />
+      <Services />
+      <Portfolio />
+      <Testimonials />
+      <Contact />
+      <Footer />
+    </>
+  )
 }
