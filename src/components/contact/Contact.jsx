c5df376a110adff4684a831433887f5a8aa28cf4
Hamam <hamam.nasrodin@appfuxion.com>
2022-09-12 15:03:17 +0700

    add form

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -48,7 +48,24 @@ function Contact() {
           </article>
         </div>
 
-        <form action=""></form>
+        <form action="">
+          <input
+            type="text"
+            name="name"
+            placeholder="Your Full Name"
+            required
+          />
+          <input type="email" name="email" placeholder="Your Email" required />
+          <textarea
+            name="message"
+            rows="7"
+            placeholder="Your Message"
+            required
+          />
+          <button type="submit" className="btn btn-primary">
+            Send Message
+          </button>
+        </form>
       </div>
     </section>
   )
