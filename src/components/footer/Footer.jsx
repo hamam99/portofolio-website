fa3cef54536a9fd300a36cbc5e3553444fd8218c
Hamam <hamam.nasrodin@appfuxion.com>
2022-09-12 15:42:11 +0700

    create footer sect

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,53 @@
 import React from 'react'
+import { FaFacebook } from 'react-icons/fa'
+import { FiInstagram } from 'react-icons/fi'
+import { IoLogoTwitter } from 'react-icons/io'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#" className="footer__logo">
+        Hamam
+      </a>
+      <ul className="permalinks">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#experience">Experience</a>
+        </li>
+        <li>
+          <a href="#services">Services</a>
+        </li>
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#testimonials">Testimonials</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+      <div className="footer__socials">
+        <a href="https://www.facebook.com">
+          <FaFacebook />
+        </a>
+        <a href="https://www.instagram.com">
+          <FiInstagram />
+        </a>
+        <a href="https://www.twitter.com">
+          <IoLogoTwitter />
+        </a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; Hamam. All rights reserved.</small>
+      </div>
+    </footer>
   )
 }
 
